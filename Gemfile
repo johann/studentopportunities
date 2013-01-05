dd1ce5a09f616a0037cbb21947fa45a110b22b57
johannkerr <johannkerr@gmail.com>
2013-01-05 16:44:57 -0430

    rails_apps_composer: initial commit

--- Gemfile
+++ Gemfile
@@ -36,14 +36,3 @@ gem 'jquery-rails'
 
 # To use debugger
 # gem 'ruby-debug19', :require => 'ruby-debug'
-
-gem "rspec-rails", ">= 2.11.4", :group => [:development, :test]
-gem "capybara", ">= 2.0.1", :group => :test
-gem "database_cleaner", ">= 0.9.1", :group => :test
-gem "email_spec", ">= 1.4.0", :group => :test
-gem "factory_girl_rails", ">= 4.1.0", :group => [:development, :test]
-gem "bootstrap-sass", ">= 2.2.2.0"
-gem "devise", ">= 2.1.2"
-gem "cancan", ">= 1.6.8"
-gem "rolify", ">= 3.2.0"
-gem "simple_form", ">= 2.0.4"
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Sop::Application.config.secret_token = '0c65447ae35d705e3ffc89610b82f1824e2076d59504e8046593d0b687af2a34528e0fc2f947254750691679c12d22b288f5a6b0f3a5f52b3e1a8a295b8a56a7'
+Sop::Application.config.secret_token = '8e9312978dfe61bfe5ffab4a4e51cc448bd04fce5c84bce6ad5436a37c6d221d2a563bbbb45f8d01a37ef059728a238e83fb6ad6240a36b0a6fcb5890b956df4'
